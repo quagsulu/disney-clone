bf1f46dea45d8ea62f66bddcdcf756226654221a
quagsulu <quangnvph24106@fpt.edu.vn>
2023-11-18 14:05:20 +0700

    slide và responesive

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header'
+import Slider from './components/Slider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<Header />
+      <Header />
+      <Slider />
     </>
   )
 }
